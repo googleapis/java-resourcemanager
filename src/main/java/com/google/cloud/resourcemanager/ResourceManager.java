b8511536ac8cc9a281b7f19986c029c37cd08c33
athakor <ajit.thakor@qlogic.io>
2020-03-27 19:11:36 +0530

    feat: update javadoc

--- src/main/java/com/google/cloud/resourcemanager/ResourceManager.java
+++ src/main/java/com/google/cloud/resourcemanager/ResourceManager.java
@@ -355,10 +355,12 @@ public interface ResourceManager extends Service<ResourceManagerOptions> {
    */
   Map<String, Boolean> testOrgPermissions(String resource, List<String> permissions);
 
-  /*
-   * Sets the access control policy on an Organization resource. Replaces any existing policy. The
-   * resource field should be the organization's resource name, e.g. "organizations/123".
+  /**
+   * Sets the access control policy on an Organization resource. Replaces any existing policy.
    *
+   * @param resource the organization's resource name, e.g. "organizations/123"
+   * @param newPolicy the new IAM policy for resource
+   * @return the updated IAM policy for resource.
    * @throws ResourceManagerException upon failure
    * @see <a href=
    *     "https://cloud.google.com/resource-manager/reference/rest/v1/organizations/setIamPolicy">
